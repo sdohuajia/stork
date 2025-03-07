a17d00602f23169c93b52e666fc899059b986158
sdohuajia <63113882+sdohuajia@users.noreply.github.com>
2025-03-07 20:32:26 +0800

    Update stork.sh

--- stork.sh
+++ stork.sh
@@ -13,15 +13,13 @@ function main_menu() {
         echo "退出脚本，请按键盘 Ctrl + C 退出即可"
         echo "请选择要执行的操作:"
         echo "1. 部署 stork 节点"
-        echo "2. 监控日志"
-        echo "3. 退出脚本"
+        echo "2. 退出脚本"
         echo "================================================================"
-        read -p "请输入选择 (1/2/3): " choice
+        read -p "请输入选择 (1/2): " choice
 
         case $choice in
             1)  deploy_stork_node ;;
-            2)  monitor_logs ;;
-            3)  exit ;;
+            2)  exit ;;
             *)  echo "无效选择，请重新输入！"; sleep 2 ;;
         esac
     done
@@ -103,68 +101,9 @@ function deploy_stork_node() {
     npm install
 
     # 启动项目，并将输出重定向到日志文件
-    screen -S stork -dm bash -c "cd ~/stork && npm start >> ~/stork/output.log 2>&1"
+    screen -S stork -dm bash -c "cd ~/stork && npm start"
 
-    echo "项目已启动，使用 'screen -r stork' 查看日志 或者打开 output.log"
-
-    # 提示用户按任意键返回主菜单
-    read -n 1 -s -r -p "按任意键返回主菜单..."
-    main_menu
-}
-
-# 日志监控函数
-function monitor_logs() {
-    MONITOR_SCRIPT="/root/stork/monitor.sh"
-    SCREEN_NAME="stork"
-    LOG_FILE="/root/stork/output.log"
-    MONITOR_LOG="/root/stork/monitor.log"
-
-    cat > "$MONITOR_SCRIPT" <<EOL
-#!/bin/bash
-echo "日志监控已启动..." > $MONITOR_LOG
-
-while true; do
-    # 检查日志文件中是否有 Network error
-    if grep -q "Network error" "$LOG_FILE"; then
-        echo "\$(date): 检测到 Network error，正在重启服务..." >> $MONITOR_LOG
-
-        # 停止进程
-        screen -S "$SCREEN_NAME" -X stuff $'\003'
-        sleep 5
-
-        # 清理日志
-        > "$LOG_FILE"
-
-        # 重新启动服务
-        screen -S "$SCREEN_NAME" -X stuff "npm start"$'\n'
-        
-        echo "\$(date): 服务已重启" >> $MONITOR_LOG
-    fi
-
-    # 检查日志文件中是否有邮箱地址
-    if grep -q -o "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}" "$LOG_FILE"; then
-        # 获取一轮所有邮箱地址，处理完一轮后清理日志
-        emails=$(grep -o "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}" "$LOG_FILE" | sort | uniq)
-
-        # 输出所有邮箱
-        echo "$(date): 检测到邮箱地址：$emails" >> $MONITOR_LOG
-
-        # 清理日志文件
-        > "$LOG_FILE"
-
-        echo "$(date): 已清理日志文件" >> $MONITOR_LOG
-    fi
-
-    # 每30秒检查一次日志文件
-    sleep 30
-done
-EOL
-
-    chmod +x "$MONITOR_SCRIPT"
-    nohup "$MONITOR_SCRIPT" > "$MONITOR_LOG" 2>&1 &
-
-    echo "日志监控已启动，后台运行中。"
-    echo "查看监控日志：tail -f $MONITOR_LOG"
+    echo "项目已启动，使用 'screen -r stork' 查看日志 "
 
     # 提示用户按任意键返回主菜单
     read -n 1 -s -r -p "按任意键返回主菜单..."
